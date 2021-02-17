c7a1cab3090449137329fec0e215a4af3eb65b64
/mahir Dhall <mahirdhall4@gmail.com>
2021-02-17 12:26:58 -0500

    Added component did mount

--- src/components/Zoom.js
+++ src/components/Zoom.js
@@ -1,19 +1,70 @@
 import React, { Component } from 'react'
 const zoom = require('@zoomus/websdk')
-zoom.ZoomMtg.setZoomJSLib('https://source.zoom.us/1.9.0/lib', '/av');
-zoom.ZoomMtg.preLoadWasm()
-zoom.ZoomMtg.prepareJssdk()
-const signature = "MHV4VHBUU0VSbm1BZkxlOE41YS1ldy45NjQ1OTAzNDI2NC4xNjEzNTA5MDE4ODQ4LjAuRHFMdVVmdFpFMDkxOGRRNmhFbTMwSzRaRURtUHA2dk5icThSZS9valltYz0="
+
+const signature = "MHV4VHBUU0VSbm1BZkxlOE41YS1ldy45OTI4MDMyMDg0OC4xNjEzNTgwNjc4NjkzLjAuamxPcVlsVDhyeHBsVTZWTXdkK1AvaTJ0WHJKQUJVSG5hTUhkbUg2eXJBRT0="
 
 export default class Zoom extends Component {
 
+    componentDidMount() {
+        zoom.ZoomMtg.setZoomJSLib('https://source.zoom.us/1.9.0/lib', '/av');
+        zoom.ZoomMtg.preLoadWasm()
+        zoom.ZoomMtg.prepareJssdk()
+    }
 
+
+
+
+    launchMeeting = () => {
+        // change state of meeting
+        // this.setState({ meetingLaunched: !this.state.meetingLaunched })
+        const meetConfig = {
+            apiKey: '0uxTpTSERnmAfLe8N5a-ew',
+            meetingNumber: 91396987690,
+            leaveUrl: 'http://192.168.1.39:3000',
+            userName: 'mahir Dhall',
+            userEmail: 'mahirdhall4@gmail.com',
+            role: 0 // 1 for host; 0 for attendee
+        };
+        // generateSignature should only be used in development
+        zoom.ZoomMtg.generateSignature({
+            meetingNumber: meetConfig.meetingNumber,
+            apiKey: meetConfig.apiKey,
+            apiSecret: meetConfig.apiSecret,
+            role: meetConfig.role,
+            success(res) {
+                console.log('signature', res.result);
+                zoom.ZoomMtg.init({
+                    leaveUrl: 'http://www.zoom.us',
+                    success() {
+                        zoom.ZoomMtg.join(
+                            {
+                                meetingNumber: meetConfig.meetingNumber,
+                                userName: meetConfig.userName,
+                                signature: res.result,
+                                apiKey: meetConfig.apiKey,
+                                userEmail: 'mahirdhll4@gmail.com',
+                                success() {
+                                    console.log('join meeting success');
+                                },
+                                error(res) {
+                                    console.log(res);
+                                }
+                            }
+                        );
+                    },
+                    error(res) {
+                        console.log(res);
+                    }
+                });
+            }
+        });
+    }
     join_meeting = () => {
         
         
         const meetConfig = {
             apiKey: '0uxTpTSERnmAfLe8N5a-ew',
-            meetingNumber: 96459034264,
+            meetingNumber: 99280320848,
             leaveUrl: 'http://192.168.1.39:3000',
             userName: 'mahir Dhall',
             userEmail: 'mahirdhall4@gmail.com',
